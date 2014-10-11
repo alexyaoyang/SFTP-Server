6124e902f04adcabe36143df8d30b7345fdf12fa
Alex Yao <alexyaoyang@hotmail.com>
2014-10-12 03:12:22 +0800

    DIR method

--- server/FTPServer.java
+++ server/FTPServer.java
@@ -15,8 +15,6 @@ class FTPServer {
 		if(!argv[1].isEmpty()){
 			dataport = Integer.parseInt(argv[1]);
 		}
-		
-		System.out.println(String.format("200 PORT $s %d \r\n",ipAddress,dataport));
 
 		while(true) {
 
@@ -28,47 +26,59 @@ class FTPServer {
 
 			commandFromClient = inFromClient.readLine(); 
 			System.out.println("client command: " + commandFromClient + "\n");
-			
+
 			if(commandFromClient.compareTo("PASV")){
 				responseFromServer = String.format("200 PORT %s %d \r\n",ipAddress,dataport);
 				outToClient.writeBytes(responseFromServer);
 				commandFromClient = inFromClient.readLine();
 			}
-			
+
 			String[] splittedCommand = commandFromClient.split(" ");
 
 			if(splittedCommand[0].compareTo("DIR")){
 				if(splittedCommand.length == 1){
-					outToClient.writeBytes("200 DIR COMMAND OK");
-					
-					outToClient.writeBytes("200 OK");
+					outToClient.writeBytes("200 DIR COMMAND OK\r\n");
+
+					String path = new File(".").getAbsolutePath();
+					File dir = new File(path);
+					String[] listFiles = dir.list();
+
+					Arrays.sort(listFiles);
+
+					responseFromServer = "";
+					for (int i = 0; i < listFiles.length; i++) {
+						responseFromServer.concat(listFiles[i]+"\r\n");
+					}
+					outToClient.writeBytes(responseFromServer);
+
+					outToClient.writeBytes("200 OK\r\n");
 				}
 				else{
-					outToClient.writeBytes("501 INVALID ARGUMENTS");
+					outToClient.writeBytes("501 INVALID ARGUMENTS\r\n");
 				}
 			}
 			else if(splittedCommand[0].compareTo("GET")){
 				if(splittedCommand.length == 2){
-					outToClient.writeBytes("200 GET COMMAND OK");
-					
-					outToClient.writeBytes("200 OK");
+					outToClient.writeBytes("200 GET COMMAND OK\r\n");
+
+					outToClient.writeBytes("200 OK\r\n");
 				}
 				else{
-					outToClient.writeBytes("501 INVALID ARGUMENTS");
+					outToClient.writeBytes("501 INVALID ARGUMENTS\r\n");
 				}
 			}
 			else if(splittedCommand[0].compareTo("PUT")){
 				if(splittedCommand.length >= 2){
-					outToClient.writeBytes("200 PUT COMMAND OK");
-					
-					outToClient.writeBytes("200 OK");
+					outToClient.writeBytes("200 PUT COMMAND OK\r\n");
+
+					outToClient.writeBytes("200 OK\r\n");
 				}
 				else{
-					outToClient.writeBytes("501 INVALID ARGUMENTS");
+					outToClient.writeBytes("501 INVALID ARGUMENTS\r\n");
 				}
 			}
 			else {
-				outToClient.writeBytes("500 UNKNOWN COMMAND");
+				outToClient.writeBytes("500 UNKNOWN COMMAND\r\n");
 			}
 			outToClient.close();
 			inFromClient.close();

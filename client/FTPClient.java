cf2275f0b5541c35c1d4e7d52ad3d18ac63d1326
Alex Yao <alexyaoyang@hotmail.com>
2014-10-12 03:35:40 +0800

    args check and output bugger

--- client/FTPClient.java
+++ client/FTPClient.java
@@ -1,36 +1,33 @@
 import java.io.*; 
-
 import java.net.*; 
 
 class FTPClient { 
 
     public static void main(String argv[]) throws Exception 
     { 
-        String sentence; 
-        String modifiedSentence; 
-
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
+        if(argv.length<3){
+        	System.out.println("Args missing");
+        	System.exit(0);
+        }
+        String serverReply = "";
+        String ipAddress = argv[0];
+        int serverPort = Integer.parseInt(argv[1]);
+        String command = argv[2];
 
         // Socket clientSocket = new Socket("suna.comp.nus.edu.sg", 6789); 
-        Socket clientSocket = new Socket("localhost", 64998); 
+        Socket clientSocket = new Socket("localhost", serverport); 
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
-
-        BufferedReader inFromServer = new BufferedReader(
-          new InputStreamReader(clientSocket.getInputStream())); 
-
-        sentence = inFromUser.readLine(); 
-        System.out.println("CLIENT (READ FROM PROMPT) " + sentence); 
-        outToServer.writeBytes(sentence + '\n'); 
-
-        modifiedSentence = inFromServer.readLine(); 
+        DataOutputStream outToServer = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream())); 
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+        
+         = inFromServer.readLine(); 
+        outToServer.writeBytes(sentence + '\r\n'); 
 
         System.out.println("CLIENT (READ FROM SERVER) " + modifiedSentence); 
 
+        inFromServer.close();
+        outToServer.close();
         clientSocket.close(); 
-
     } 
 }
 

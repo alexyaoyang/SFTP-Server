82ddbcfc2f955db6616e2dec3fa109ba12267749
Alex Yao <alexyaoyang@hotmail.com>
2014-10-12 22:53:26 +0800

    client dir method

--- client/FTPClient.java
+++ client/FTPClient.java
@@ -2,34 +2,104 @@ import java.io.*;
 import java.net.*; 
 
 class FTPClient { 
+	public static String WRITEPATH = "./client-directory/";
+	public int controlPort;
+	public int dataPort;
+	public String ipAddress;
+	public String command;
+	public Socket serverControlSocket;
+	public PrintWriter outToControlSocket;
+	public BufferedReader inFromControlSocket;
 
-    public static void main(String argv[]) throws Exception 
-    { 
-        if(argv.length<3){
-        	System.out.println("Args missing");
-        	System.exit(0);
-        }
-        String serverReply = "";
-        String ipAddress = argv[0];
-        int serverPort = Integer.parseInt(argv[1]);
-        String command = argv[2];
+	public static void main(String argv[]) throws IOException{ 
+		FTPClient FC = new FTPClient();
+		FC.setupClient(argv);
+		FC.runClient();
+	}
 
-        // Socket clientSocket = new Socket("suna.comp.nus.edu.sg", 6789); 
-        Socket clientSocket = new Socket("localhost", serverport); 
+	public void runClient() throws IOException{
+		// Socket clientSocket = new Socket("suna.comp.nus.edu.sg", 6789); 
+		serverControlSocket = new Socket(ipAddress, controlPort); 
 
-        DataOutputStream outToServer = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream())); 
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
-        
-         = inFromServer.readLine(); 
-        outToServer.writeBytes(sentence + '\r\n'); 
+		outToControlSocket = new PrintWriter(serverControlSocket.getOutputStream(), true);
+		inFromControlSocket = new BufferedReader(new InputStreamReader(serverControlSocket.getInputStream()));
 
-        System.out.println("CLIENT (READ FROM SERVER) " + modifiedSentence); 
+		System.out.println("Sending PASV");
+		outToControlSocket.println("PASV");
 
-        inFromServer.close();
-        outToServer.close();
-        clientSocket.close(); 
-    } 
+		String serverReply = inFromControlSocket.readLine();
+		System.out.println("received from server: "+serverReply);
+		String[] splitReplyString = serverReply.split(" ");
+
+		createWriteDirectory();
+
+		if(splitReplyString[0].equals("200")){
+			ipAddress = splitReplyString[2];
+			dataPort = Integer.parseInt(splitReplyString[3]);
+
+			outToControlSocket.println(command);
+			serverReply = inFromControlSocket.readLine();
+			if(serverReply.split(" ")[0].equals("200")){
+				if(command.equals("DIR")){
+					renderDIR();
+				}
+				else if(command.equals("GET")){
+
+				}
+				else if(command.equals("PUT")){
+
+				}
+			}
+		}
+		inFromControlSocket.close();
+		outToControlSocket.close();
+		serverControlSocket.close(); 
+	}
+
+	public void renderDIR() throws IOException{
+		Socket serverDataSocket = new Socket(ipAddress, dataPort);
+		BufferedReader inFromDataSocket = new BufferedReader(new InputStreamReader(serverDataSocket.getInputStream()));
+
+		File dirList = new File(WRITEPATH+"directory_listing");
+
+		if (!dirList.exists()) {
+			dirList.createNewFile();
+		}
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(dirList.getAbsoluteFile()));
+		
+		String dirListString;
+		while((dirListString = inFromDataSocket.readLine()) != null){
+			System.out.println(dirListString);
+			bw.write(dirListString+"\r\n");
+		}
+		bw.flush();
+		bw.close();
+		
+		if(inFromControlSocket.readLine().split(" ")[0].equals("200")){
+			inFromDataSocket.close();
+			serverDataSocket.close();
+		}
+	}
+
+	public void createWriteDirectory() throws IOException{
+		File writeDir = new File(WRITEPATH);
+
+		if (!writeDir.exists()) {
+			writeDir.mkdir();
+		}
+	}
+
+	public void setupClient(String argv[]) throws IOException{
+		if(argv.length<3){
+			System.out.println("Args missing");
+			System.exit(0);
+		}
+		ipAddress = argv[0];
+		controlPort = Integer.parseInt(argv[1]);
+		command = argv[2];
+	}
 }
 
- 
+
 
